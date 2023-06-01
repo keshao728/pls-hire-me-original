e8e75c1e758980ada36b30ce4318c21f2d5ca5a1
Kelly <kellyshao728@gmail.com>
2023-06-01 14:10:53 -0700

    Port change for heroku

--- app/app.py
+++ app/app.py
@@ -124,4 +124,6 @@ def process_data():
 
 
 if __name__ == '__main__':
-    app.run(port=5010)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5010))
+    app.run(host='0.0.0.0', port=port)

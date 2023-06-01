05501e20eca63fba6c63154fed30dc51ab45ba71
Kelly <kellyshao728@gmail.com>
2023-06-01 14:57:10 -0700

    static/html fix

--- app/app.py
+++ app/app.py
@@ -27,10 +27,11 @@ CORS(app)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
+
 
 
 
